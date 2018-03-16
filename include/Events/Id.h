ba906b13149bbbd5207963fe4b65523596a836d1
Wayne Larson Jr <wlarson30@gmail.com>
2018-03-16 02:49:03 -0400

    Added Event/Id.h States/Id.h

---
+++ include/Events/Id.h
@@ -0,0 +1,12 @@
+#ifndef ID_H_INCLUDED
+#define ID_H_INCLUDED
+
+namespace Events
+{
+    enum Id
+    {
+
+    }
+}
+
+#endif // ID_H_INCLUDED
---
+++ include/States/Id.h
@@ -0,0 +1,12 @@
+#ifndef ID_H_INCLUDED
+#define ID_H_INCLUDED
+
+namespace States
+{
+    enum Id
+    {
+
+    }
+}
+
+#endif // ID_H_INCLUDED
