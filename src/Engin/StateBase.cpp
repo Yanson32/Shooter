196c95df13715dee1ea3b73341e0162a4bb5cc8a
Wayne Larson Jr <wlarson30@gmail.com>
2018-04-08 04:50:36 -0400

    Minor update

--- src/Engin/StateBase.cpp
+++ src/Engin/StateBase.cpp
@@ -3,6 +3,8 @@
 #include <SFML/Window/Event.hpp>
 #include "Settings.h"
 #include <GameUtilities/Engin/Engin.h>
+#include "Events/Id.h"
+#include "Events/Events.h"
 
 sf::RenderWindow StateBase::window(sf::VideoMode({800, 600}), Settings::inst().getTitle());
 tgui::Gui StateBase::gui(window);
@@ -15,7 +17,16 @@ StateBase::StateBase()
 
 void StateBase::handleGUEvent(GU::Engin::Engin& engin, GU::Evt::EventPtr event)
 {
-    std::cout << "Gu event " << std::endl;
+    switch(event->id)
+    {
+        case Events::Id::PUSH_STATE:
+            std::shared_ptr<GU::Evt::PushState> temp =  std::dynamic_pointer_cast<GU::Evt::PushState>(event);
+            if(temp)
+            {
+
+            }
+        break;
+    }
 }
 
 void StateBase::handleSFEvent(GU::Engin::Engin& engin, const sf::Event &event)
