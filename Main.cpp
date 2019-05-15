1f9a5ba266c25a8436307e571e5a6b2d1370d143
Wayne Larson Jr <wlarson30@gmail.com>
2019-05-15 19:19:08 -0300

    Updated main

--- Main.cpp
+++ Main.cpp
@@ -3,36 +3,38 @@
 #include "States/IntroState.h"
 #include "Events/EventManager.h"
 #include <memory>
-
+#include <TGUI/TGUI.hpp>
 int main()
 {
-
 	sf::RenderWindow window(sf::VideoMode(200, 200), "Shooter");
+
+	tgui::Gui gui(window);
+
     sf::Clock timer;
     const sf::Time deltaTime = sf::seconds(1.0f / 60.0f);
     sf::Time accumulator = sf::seconds(0);
 
-	//Game engin;
+	Game engin;
 
-	//std::unique_ptr<IntroState> state(new IntroState(window));
-	//engin.ChangeState(std::move(state));
+	std::unique_ptr<IntroState> state(new IntroState(window, gui));
+	engin.ChangeState(std::move(state));
 
 	//EventManager::inst().Post<PlayMusic>("../Resources/Music/Electro_Zombies.ogg");
 	//EventManager::inst().Post<MusicVolumeChanged>();
 	//EventManager::inst().Post<SoundVolumeChanged>();
 
 
-    //while (engin.IsRunning())
-    //{
-        //accumulator += timer.restart();
-        //engin.HandleEvents(deltaTime.asSeconds());
-        //while(accumulator.asSeconds() >= deltaTime.asSeconds())
-        //{
-            //engin.Update(deltaTime.asSeconds());
-            //accumulator -= deltaTime;
-        //}
-        //engin.Draw(deltaTime.asSeconds());
-    //}
+    while (engin.IsRunning())
+   	{
+        accumulator += timer.restart();
+        engin.HandleEvents(deltaTime.asSeconds());
+        while(accumulator.asSeconds() >= deltaTime.asSeconds())
+        {
+            engin.Update(deltaTime.asSeconds());
+            accumulator -= deltaTime;
+        }
+        engin.Draw(deltaTime.asSeconds());
+    }
 
 
     return 0;
