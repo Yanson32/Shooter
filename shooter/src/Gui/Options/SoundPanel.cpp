505f09403a1c84bfd7d0f73f170c7eb24a0da05c
Yanson <wlarson30@gmail.com>
2019-12-01 01:27:31 -0500

    Refactor: Changed the color of the sliders in the sound panel

--- shooter/src/Gui/Options/SoundPanel.cpp
+++ shooter/src/Gui/Options/SoundPanel.cpp
@@ -8,6 +8,8 @@ SoundPanel::SoundPanel()//:
 //OptionsPanel(Gui::id::SOUND)
 {
     //ctor
+    sf::Color trackColor = sf::Color(75, 75, 75);
+    sf::Color thumbColor = sf::Color(100, 100, 100);
     effectSlider = this->appendSlider("Effects");
     effectSlider->setValue(Settings::effectVolume);
     effectSlider->connect("ValueChanged", [&](){
@@ -15,13 +17,17 @@ SoundPanel::SoundPanel()//:
         Settings::effectVolume = volume;
     });
 
-
+    effectSlider->getRenderer()->setTrackColor(trackColor);
+    effectSlider->getRenderer()->setThumbColor(thumbColor);
     musicSlider = this->appendSlider("Music");
     musicSlider->setValue(Settings::musicVolume);
     musicSlider->connect("ValueChanged", [&](){
         int volume = musicSlider->getValue();
         Settings::musicVolume = volume;
     });
+
+    musicSlider->getRenderer()->setTrackColor(trackColor);
+    musicSlider->getRenderer()->setThumbColor(thumbColor);
 
     std::vector<sf::String> music = {"Music 1", "Music 2", "Music 3"};
     currentMusic = this->appendComboBox("Current Music", music);
